3dace3e777fcbd3201e3d9f5a34ad6aa205ac488
Богдан Воробйовський <piiiko@ukr.net>
2019-10-27 20:54:19 +0200

    Initial Commit

---
+++ MWDNTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MWDNTest
+//
+//  Created by Богдан Воробйовський on 27.10.2019.
+//  Copyright © 2019 Богдан Воробйовський. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
